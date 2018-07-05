4323eff8b4f9782a6ff55418ed8aa5a23b1380a7
Stavros Kounis <skounis@gmail.com>
2018-07-05 09:03:46 +0300

    chore(config) Add heroku URL

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://192.168.1.24:9000/';
+export const apiUrl = 'https://rest-contacts.herokuapp.com/'; //'http://192.168.1.24:9000/';
 export const MASTER_TOKEN = 'pKiTGTGlJEXkgjXJbTSI7oTWHsGKGPrB';
